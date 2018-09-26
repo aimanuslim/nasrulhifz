e5bb3ecb4639ef7de47cdd8bff3525b826bff533
Aiman Md Uslim <aiman.muslim@petronas.com.my>
2018-09-26 15:14:30 +0800

    set debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l6p==m@t^9z_!xc0-y)e8*cz@ukno1nvm_913#4)up=$-hwio1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'protected-thicket-37721.herokuapp.com']
 
