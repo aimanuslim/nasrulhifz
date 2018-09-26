ad0efd76c852ecc18eed4a16523d7cec3461f62e
Aiman Md Uslim <aiman.muslim@petronas.com.my>
2018-09-26 13:56:50 +0800

    added staticfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -139,4 +139,3 @@ LOGIN_REDIRECT_URL = '/nasrulhifz/'
 # import django_heroku
 # django_heroku.settings(locals())
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
