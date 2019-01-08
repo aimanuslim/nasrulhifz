e044e083075e14d186153fb534618786b7d5c85a
Aiman Uslim <aimanuslim@gmail.com>
2019-01-08 17:02:22 +0800

    hifz list working properly. next add update refrshed and tickboxes

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '10.12.242.122', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '10.12.29.37', 'localhost']
 
 
 
