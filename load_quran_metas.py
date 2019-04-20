16c6120981da3ddeb3778a0a70ad673244edd596
Aiman Md Uslim <aimanuslim@gmail.com>
2019-04-20 08:53:58 +0800

    Added new scripts for dev

--- load_quran_metas.py
+++ load_quran_metas.py
@@ -1,4 +1,4 @@
-from nasrulhifz.models import QuranMeta, SurahMeta, User, Hifz
+from nasrulhifz.models import QuranMeta, SurahMeta, User, Hifz, WordIndex
 
 
 with open('quran-uthmani.txt', 'r', encoding='utf8') as f:
@@ -13,9 +13,9 @@ strings = []
 for ayat in text[:6235]:
     ayat = ayat.strip()
     surah_number, ayat_number, string = ayat.split('|')
-    for i, range in enumerate(juz_ranges):
-        if range.get(surah_number) is not None:
-            ayat_limit = range.get(surah_number)
+    for i, myrange in enumerate(juz_ranges):
+        if myrange.get(surah_number) is not None:
+            ayat_limit = myrange.get(surah_number)
             if int(ayat_number) <= ayat_limit[1] and int(ayat_number) >= ayat_limit[0]:
                 qm = QuranMeta(ayat_string=string, surah_number=surah_number, ayat_number=ayat_number, juz_number=i)
                 qm.save()
@@ -32,7 +32,8 @@ for sm in surahmeta:
     osm.save()
 
 
-u = User(username='aimanuslim', password='chanayya211')
+u = User(username='aimanuslim')
+u.set_password('chanayya211')
 u.is_staff = True
 u.is_superuser = True
 
@@ -40,10 +41,33 @@ u.save()
 
 from numpy.random import randint
 for i in range(15):
-    sn = randint(114)
+    sn = randint(114) + 1
     lastayat = SurahMeta.objects.get(surah_number=sn).surah_ayat_max
-    h = Hifz(hafiz=u, surah_number=sn, ayat_number=randint(lastayat))
+    an = randint(lastayat) + 1
+
+    found_unique = False
+    while not found_unique:
+        try:
+            htest =Hifz.objects.get(surah_number=sn, ayat_number=an)
+            sn = randint(114) + 1
+            lastayat = SurahMeta.objects.get(surah_number=sn).surah_ayat_max
+            an = randint(lastayat) + 1
+        except:
+            found_unique = True
+    h = Hifz(hafiz=u, surah_number=sn, ayat_number=an)
+
+    h.average_difficulty = 3
     h.save()
+
+    lenayat = len(QuranMeta.objects.get(surah_number=sn, ayat_number=an).ayat_string.split(" "))
+    for i in range(lenayat):
+        w = WordIndex(index=i, difficulty=randint(3)+1, hifz=h)
+        w.save()
+
+    h.save_average_difficulty()
+    h.save()
+
+
 
 
 
---
+++ sample_data_load.py
@@ -0,0 +1,36 @@
+from nasrulhifz.models import QuranMeta, SurahMeta, User, Hifz, WordIndex
+
+u = User(username='aimanuslim')
+u.set_password('chanayya211')
+u.is_staff = True
+u.is_superuser = True
+
+u.save()
+
+from numpy.random import randint
+for i in range(15):
+    sn = randint(114) + 1
+    lastayat = SurahMeta.objects.get(surah_number=sn).surah_ayat_max
+    an = randint(lastayat) + 1
+
+    found_unique = False
+    while not found_unique:
+        try:
+            htest =Hifz.objects.get(surah_number=sn, ayat_number=an)
+            sn = randint(114) + 1
+            lastayat = SurahMeta.objects.get(surah_number=sn).surah_ayat_max
+            an = randint(lastayat) + 1
+        except:
+            found_unique = True
+    h = Hifz(hafiz=u, surah_number=sn, ayat_number=an)
+
+    h.average_difficulty = 3
+    h.save()
+
+    lenayat = len(QuranMeta.objects.get(surah_number=sn, ayat_number=an).ayat_string.split(" "))
+    for i in range(lenayat):
+        w = WordIndex(index=i, difficulty=randint(3)+1, hifz=h)
+        w.save()
+
+    h.save_average_difficulty()
+    h.save()
