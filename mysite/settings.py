36189019747b10194939bc6fa5aaa6934e77bcaf
Aiman <aimanuslim@gmail.com>
2021-09-15 16:02:18 +0800

    modify statifile path on master

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,9 +132,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "nasrulhifz/static"),
-    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
