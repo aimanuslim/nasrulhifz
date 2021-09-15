fd115cf1315cbb4d42ddf1201c01068ccd12412b
Aiman <aimanuslim@gmail.com>
2021-09-15 15:19:58 +0800

    Removed local settings from master

--- mysite/settings.py
+++ mysite/settings.py
@@ -168,7 +168,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # try to load local_settings.py if it exists
 try:
-    from .local_settings import *
+    from local_settings import *
 except Exception as e:
     import dj_database_url
 
